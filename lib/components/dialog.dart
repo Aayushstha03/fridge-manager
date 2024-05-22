efdb8cc654d8ecbeafae17f2ea4f147a9e4448d0
Aayush Shrestha <Aayushshrestha2003@gmail.com>
2024-05-22 19:12:03 +0545

    final date duration addition as well

--- lib/components/dialog.dart
+++ lib/components/dialog.dart
@@ -20,7 +20,9 @@ class AddNewRecipeDialog extends StatefulWidget {
 class _AddNewRecipeDialogState extends State<AddNewRecipeDialog> {
   DateTime selectedDate = DateTime.now();
   final TextEditingController itemController = TextEditingController();
-  TextEditingController dateController = TextEditingController();
+  TextEditingController purchaseDateController = TextEditingController();
+  TextEditingController expiryDateController = TextEditingController();
+  TextEditingController expiryDurationController = TextEditingController();
   //get location to AppData directory assigned by the OS to the app
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
@@ -56,7 +58,7 @@ class _AddNewRecipeDialogState extends State<AddNewRecipeDialog> {
     return file.writeAsString('$pantryContents');
   }
 
-  Future<void> _selectDate(BuildContext context) async {
+  Future<void> _selectPurchaseDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
         context: context,
         initialDate: selectedDate,
@@ -66,9 +68,28 @@ class _AddNewRecipeDialogState extends State<AddNewRecipeDialog> {
       setState(() {
         selectedDate = picked;
         FocusManager.instance.primaryFocus?.unfocus();
-        dateController.text = selectedDate.toLocal().toString().split(' ')[0];
+        purchaseDateController.text =
+            selectedDate.toLocal().toString().split(' ')[0];
       });
     }
+  }
+
+  Future<void> _selectExpiryDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        FocusManager.instance.primaryFocus?.unfocus();
+        expiryDateController.text =
+            selectedDate.toLocal().toString().split(' ')[0];
+      });
+    }
+    expiryDurationController.text =
+        '${int.parse(expiryDateController.text.substring(8, 10)) - int.parse(purchaseDateController.text.substring(8, 10))} days';
   }
 
   @override
@@ -103,11 +124,8 @@ class _AddNewRecipeDialogState extends State<AddNewRecipeDialog> {
                 TextField(
                   keyboardType: TextInputType.none,
                   readOnly: true,
-                  controller: dateController,
-                  onTapOutside: (event) {
-                    FocusManager.instance.primaryFocus?.unfocus();
-                  },
-                  onTap: () => {_selectDate(context)},
+                  controller: purchaseDateController,
+                  onTap: () => {_selectPurchaseDate(context)},
                   decoration: const InputDecoration(
                     prefixIcon: Icon(Icons.calendar_today_outlined),
                     labelText: 'Purchase Date',
@@ -115,82 +133,107 @@ class _AddNewRecipeDialogState extends State<AddNewRecipeDialog> {
                     border: OutlineInputBorder(),
                   ),
                 ),
+                const SizedBox(height: 10),
 
-                //debugging
-                // Text('You selected ${itemController.text}'),
+                TextField(
+                  keyboardType: TextInputType.none,
+                  readOnly: true,
+                  controller: expiryDateController,
+                  onTap: () => {_selectExpiryDate(context)},
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.calendar_today_outlined),
+                    labelText: 'Expiry Date',
+                    hintText: 'yyyy-mm-dd',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                TextField(
+                  controller: expiryDurationController,
+                  canRequestFocus: false,
+                  enableInteractiveSelection: false,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.timer_outlined),
+                    labelText: 'Expiring after',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
                 const SizedBox(height: 10),
                 //action buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Icon(Icons.cancel_outlined),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (itemController.text.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            duration: Durations.short4,
-                            showCloseIcon: true,
-                            closeIconColor: Colors.white,
-                            backgroundColor:
-                                Theme.of(context).colorScheme.inversePrimary,
-                            content: const Text(
-                              'No Item Selected!',
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ));
-                          return;
-                        }
-
-                        Ingredient toAdd = value.getIngredients().firstWhere(
-                            (element) => element.name == itemController.text);
-
-                        // IngredientPlus(ingredient: toAdd, dateAddToPantry: dateAddToPantry, timeToExpire: timeToExpire) toCheck;
-                        if (value.getPantryContents().contains(toAdd) == true) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              duration: Durations.long1,
-                              showCloseIcon: true,
-                              closeIconColor: Colors.black,
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.inversePrimary,
-                              content: const Text(
-                                'Item already in Pantry!',
-                                style: TextStyle(color: Colors.black),
-                              ),
-                            ),
-                          );
-                        } else {
-                          value.addToPantry(IngredientPlus(
-                              ingredient: toAdd,
-                              dateAddToPantry: DateTime.now(),
-                              timeToExpire: Durations.extralong4));
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              showCloseIcon: true,
-                              closeIconColor: Colors.black,
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.inversePrimary,
-                              content: const Text(
-                                'Item successfully added to Pantry!',
-                                style: TextStyle(color: Colors.black),
-                              ),
-                            ),
-                          );
-                        }
-                        Navigator.of(context).pop();
-                      },
-                      child: const Icon(Icons.add_outlined),
-                    ),
-                  ],
-                ),
               ],
             ),
           ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton.extended(
+              heroTag: null,
+              onPressed: () => {Navigator.of(context).pop()},
+              label: const Text('Cancel'),
+              icon: const Icon(Icons.arrow_back_outlined),
+            ),
+            const SizedBox(height: 10),
+            FloatingActionButton.extended(
+                heroTag: null,
+                onPressed: () {
+                  if (itemController.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      duration: Durations.short4,
+                      showCloseIcon: true,
+                      closeIconColor: Colors.white,
+                      backgroundColor:
+                          Theme.of(context).colorScheme.inversePrimary,
+                      content: const Text(
+                        'No Item Selected!',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ));
+                    return;
+                  }
+
+                  Ingredient toAdd = value.getIngredients().firstWhere(
+                      (element) => element.name == itemController.text);
+
+                  // IngredientPlus(ingredient: toAdd, dateAddToPantry: dateAddToPantry, timeToExpire: timeToExpire) toCheck;
+                  if (value.getPantryContents().contains(toAdd) == true) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        duration: Durations.long1,
+                        showCloseIcon: true,
+                        closeIconColor: Colors.black,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        content: const Text(
+                          'Item already in Pantry!',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    );
+                  } else {
+                    value.addToPantry(IngredientPlus(
+                        ingredient: toAdd,
+                        dateAddToPantry: DateTime.now(),
+                        timeToExpire: Durations.extralong4));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        showCloseIcon: true,
+                        closeIconColor: Colors.black,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.inversePrimary,
+                        content: const Text(
+                          'Item successfully added to Pantry!',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    );
+                  }
+                  Navigator.of(context).pop();
+                },
+                label: const Text('Save'),
+                icon: const Icon(Icons.save_outlined)),
+          ],
         ),
       ),
     );
